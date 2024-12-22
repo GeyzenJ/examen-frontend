8579942a3fb3d51dd3db5060b7b89186f95d7c97
GeyzenJ <jangeyzen@hotmail.com>
2024-12-22 23:24:22 +0100

    weergave gebruikersgegevens

--- src/components/PageUserInfo.vue
+++ src/components/PageUserInfo.vue
@@ -4,13 +4,51 @@
             Gegevens gebruiker
         </h1>
 
+        <div v-if="isIngelogd">
+           <div v-for="(gegevens, index) in userInfo" :key="index" >   
+                <p>  {{ gegevens.First_Name }} {{ gegevens.Name }} </p>
+                <p> {{ gegevens.Mail }} </p>
+                <p v-if="gegevens.Admin === 1"> Campingbeheerder </p>
+         </div>
+        </div>
+
+        <div v-if="!isIngelogd">
+            <p>
+                niet ingelogd!
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'PageUserInfo'
-    
+        name: 'PageUserInfo',
+        data() {
+            return {
+                userId: null,
+                isIngelogd: false,
+                userInfo: []
+            };
+        },
+        methods: {
+            fetchUserInfo() {
+                fetch("http://localhost:3000/api/user/" + this.userId)
+                .then(response => response.json())
+                .then(data => {
+                    this.userInfo = data;
+                })
+            },
+            checkLoginStatus() {
+                this.userId = this.$cookies.get('userId');
+                if (this.userId)
+                {
+                    this.isIngelogd = true;
+                    this.fetchUserInfo();
+                }
+            }
+        },
+        created() {
+            this.checkLoginStatus();
+        }
     }
-    
 </script>
